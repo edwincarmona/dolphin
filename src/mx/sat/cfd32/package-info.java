a3d579a5d4cd7a7e3d74c4f7775bfe958b80a9ca
Edwin Carmona <eedwincarmona@gmail.com>
2019-09-12 12:51:27 -0500

    Cfd v3.2

---
+++ src/mx/sat/cfd32/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2019.09.12 a las 12:50:50 PM CDT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.sat.gob.mx/cfd/3", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package mx.sat.cfd32;
